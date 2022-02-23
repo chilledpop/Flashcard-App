184a576739110376f34da11343d216190cedbdc8
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-02-23 15:26:46 -0800

    adjusted CSS styling

--- src/Home/DeckDisplay.js
+++ src/Home/DeckDisplay.js
@@ -2,55 +2,47 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import { deleteDeck } from "../utils/api/index"
 
-function DeckDisplay({ deck, updateDecks }) {
-  const { id, name, description, cards } = deck; 
+function DeckDisplay({ id, name, description, cards, updateDecks }) {
   const history = useHistory();
 
-  const handleDelete = async(deckId) => {
+  const handleDelete = async () => {
     const userResponse = window.confirm(
       "Delete this deck?\nYou will not be able to recover it."
     );
     
     if (userResponse) {
-      await deleteDeck(deckId);
+      await deleteDeck(id);
       updateDecks(-1);
       history.push("/");
     }
   };
 
   return (
-    <div className="card" key={id}>
-      <div className="row">
-        <div className="col text-left">
-          <h5 className="card-title">{name}</h5>
+    <div className="card mb-3">
+      <div className="row card-body px-4" style={{display: "flex"}}>
+        <div className="col">
+          <h3 className="card-title">{name}</h3>
         </div>
-        <div className="col text-right">
-          <p className="card-text">{cards.length} cards</p>
+        <div className="col">
+          <p>{cards.length} cards</p>
         </div>
       </div>
-      <div className="row">
+      <div className="row px-5" style={{display: "flex"}}>
         <p className="card-text">{description}</p>
       </div>
-      <div className="row">
-        <Link to={`/decks/${id}`}>
-          <button className="btn btn-secondary">
-            <i className="bi bi-eye-fill"></i>
-            View
-          </button>
+      <div className="row card-body px-5" style={{display: "flex"}}>
+        <Link className="btn btn-secondary" to={`/decks/${id}`}>
+          View
         </Link>
-        <Link to={`/decks/${id}/study`}>
-          <button className="btn btn-primary">
-            <i className="bi bi-journal-bookmark-fill"></i>
-            Study
-          </button>
+        <Link className="btn btn-primary" to={`/decks/${id}/study`}>
+          Study
         </Link>
         <button className="btn btn-danger" onClick={handleDelete}>
-          <i className="bi bi-trash"></i>
           Delete
         </button>
       </div>
     </div>
-  )
+  );
 }
 
 
